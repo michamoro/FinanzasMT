8fcf6a16f5d408f29f12847b732f1d0c5b218b27
Tavo <guarinco@bancolombia.com.co>
2021-09-09 20:45:22 -0500

    Switch between activities

--- app/src/main/java/com/fmt/finanzasmt/MainActivity.java
+++ app/src/main/java/com/fmt/finanzasmt/MainActivity.java
@@ -1,17 +1,21 @@
 package com.fmt.finanzasmt;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Exit(View view){
+        Intent exit = new Intent(this, LoginActivity.class);
+        startActivity(exit);
     }
 }
