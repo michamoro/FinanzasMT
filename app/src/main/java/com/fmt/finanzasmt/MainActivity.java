d9c90da486bd28570c953ccd4aee1d17a0829de4
Tavo <guarinco@bancolombia.com.co>
2021-09-09 19:31:34 -0500

    Login activity

--- app/src/main/java/com/fmt/finanzasmt/MainActivity.java
+++ app/src/main/java/com/fmt/finanzasmt/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 }
